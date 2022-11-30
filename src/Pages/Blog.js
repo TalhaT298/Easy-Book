5e5a499699d71eb49d6a9d928f00ab3e598bf097
Talha <mdtalha2008@gmail.com>
2022-11-30 17:26:04 +0600

    blog update

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -52,15 +52,15 @@ In many cases, a lot of major parts of our application rely upon accessing URL s
 There are undoubtedly more pieces of state that we could identify, but these are the major categories worth focusing on for most applications you build. </p>
 			</details>
 			<details className="w-full border rounded-lg">
-				<summary className="px-4 py-6 focus:outline-none text-2xl focus-visible:ring-pink-400">How does prototypical inheritance work?</summary>
+				<summary className="px-4 py-6 focus:outline-none text-2xl focus-visible:ring-rose-900">How does prototypical inheritance work?</summary>
 				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object. </p>
 			</details>
 			<details className="w-full border rounded-lg">
-				<summary className="px-4 py-6 text-2xl focus:outline-none focus-visible:ring-pink-400">What is a unit test? Why should we write unit tests?</summary>
+				<summary className="px-4 py-6 text-2xl focus:outline-none focus-visible:ring-rose-900">What is a unit test? Why should we write unit tests?</summary>
 				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">The main objective of unit testing is to isolate written code to test and determine if it works as intended. Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages. </p>
 			</details>
             <details className="w-full border rounded-lg">
-				<summary className="px-4 text-2xl py-6 focus:outline-none focus-visible:ring-pink-400">React vs. Angular vs. Vue?</summary>
+				<summary className="px-4 text-2xl py-6 focus:outline-none focus-visible:ring-rose-900">React vs. Angular vs. Vue?</summary>
 				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
                  React
                     <br/><br/>
