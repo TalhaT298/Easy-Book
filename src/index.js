19daa7566892b7db816a9daf2d42371fe988785b
Talha <mdtalha2008@gmail.com>
2023-12-11 00:36:31 +0600

    little update

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ root.render(
   </QueryClientProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a functio
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
